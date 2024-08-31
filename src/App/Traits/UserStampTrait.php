9fac011926ab556b1e1ef323ca62822f8ec694d1
Julio C. Rodriguez <jcrodsolutions@gmail.com>
2024-08-30 22:45:40 -0500

    WIP bugfix in trait trying to identify active,createdBy and updatedBy. Also including creator and updater relationships

--- src/App/Traits/UserStampTrait.php
+++ src/App/Traits/UserStampTrait.php
@@ -12,15 +12,35 @@ trait UserStampTrait
     protected static $createdBy = 'creado_por';
     protected static $updatedBy = 'actualizado_por';
      */
+
+    public function initializeUserStampTrait()
+    {
+        $columns = Schema::getColumnListing($this->getTable());
+
+        $strActive = property_exists(static::class, 'active') ? self::$active : config('user-stamp.active', 'active');
+        $strCreadoPor = property_exists(static::class, 'createdBy') ? self::$createdBy : config('user-stamp.created_by', 'created_by');
+        $strActualizadoPor = property_exists(static::class, 'updatedBy') ? self::$updatedBy : config('user-stamp.updated_by', 'updated_by');
+
+        if (in_array($strActive, $columns)) {
+            $this->fillable[] = $strActive;
+        }
+        if (in_array($strCreadoPor, $columns)) {
+            $this->fillable[] = $strCreadoPor;
+        }
+        if (in_array($strActualizadoPor, $columns)) {
+            $this->fillable[] = $strActualizadoPor;
+        }
+    }
+
     protected static function booted()
     {
         if (!auth()->check()) {
             return;
         }
 
-        $strActive = config('user-stamp.active','active');
-        $strCreadoPor = config('user-stamp.created_by','created_by');
-        $strActualizadoPor = config('user-stamp.updated_by','updated_by');
+        $strActive = config('user-stamp.active', 'active');
+        $strCreadoPor = config('user-stamp.created_by', 'created_by');
+        $strActualizadoPor = config('user-stamp.updated_by', 'updated_by');
 
         $active = property_exists(static::class, 'active') ? self::$active : $strActive;
         $creadoPor = property_exists(static::class, 'createdBy') ? self::$createdBy : $strCreadoPor;
@@ -30,15 +50,12 @@ trait UserStampTrait
             $user_id = auth()->id();
             $columns = Schema::getColumnListing($model->getTable());
 
-            if (in_array($active,$columns)) {
-                $model->fillable = array_merge($model->fillable, [$active]);
-            }
-            if (in_array($creadoPor,$columns)) {
-                $model->fillable = array_merge($model->fillable, [$creadoPor]);
+            if (in_array($creadoPor, $columns)) {
+                // $model->fillable = array_merge($model->fillable, [$creadoPor]);
                 $model->$creadoPor = $user_id;
             }
-            if (in_array($actualizadoPor,$columns)) {
-                $model->fillable = array_merge($model->fillable, [$actualizadoPor]);
+            if (in_array($actualizadoPor, $columns)) {
+                // $model->fillable = array_merge($model->fillable, [$actualizadoPor]);
                 $model->$actualizadoPor = $user_id;
             }
         });
@@ -46,13 +63,31 @@ trait UserStampTrait
         static::updating(function ($model) use ($active, $actualizadoPor) {
             $user_id = auth()->id();
             $columns = Schema::getColumnListing($model->getTable());
-            if (in_array($active,$columns)) {
-                $model->fillable = array_merge($model->fillable, [$active]);
-            }
-            if (in_array($actualizadoPor,$columns)) {
-                $model->fillable = array_merge($model->fillable, [$actualizadoPor]);
+            if (in_array($actualizadoPor, $columns)) {
+                // $model->fillable = array_merge($model->fillable, [$actualizadoPor]);
                 $model->$actualizadoPor = $user_id;
             }
         });
+    }
+
+    /**
+     * Define the relationship with the user model for the `created_by` field.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function creator(): belongsTo
+    {
+        $userModelClass = config('auth.providers.users.model', \App\Models\User::class);
+        return $this->belongsTo($userModelClass, 'created_by');
+    }
+    /**
+     * Define the relationship with the user model for the `created_by` field.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function updater(): belongsTo
+    {
+        $userModelClass = config('auth.providers.users.model', \App\Models\User::class);
+        return $this->belongsTo($userModelClass, 'updated_by');
     }
 }
